4b84bf36ac1cc5d55ce44de05d5031f242338720
SBM <SBM@SKM>
2022-08-22 12:49:24 +0530

    Add project files.

---
+++ Phoneword/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿namespace Phoneword;
+
+public partial class MainPage : ContentPage
+{
+	string translatedNumber;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	public void OnTranslate(object sender, EventArgs e)
+	{
+		string enteredNumber = PhoneNumberText.Text;
+		translatedNumber = Core.PhonewordTranslator.ToNumber(enteredNumber);
+
+		if (!string.IsNullOrEmpty(translatedNumber))
+		{
+			CallButton.IsEnabled = true;
+			CallButton.Text = $"Call {translatedNumber}";
+		}
+		else
+		{
+			CallButton.IsEnabled = false;
+			CallButton.Text = "Call";
+		}
+	}
+
+	async void OnCall(object sender, EventArgs e)
+	{
+		if (await this.DisplayAlert(
+			"Dial a Number",
+			$"Would you like to call {translatedNumber}?",
+			"Yes",
+			"No"))
+		{
+			try
+			{
+				if (PhoneDialer.Default.IsSupported)
+					PhoneDialer.Default.Open(translatedNumber);
+			}
+			catch (ArgumentNullException)
+			{
+				await DisplayAlert("Unable to Dial", "Phone number was not valid", "OK");
+			}
+			catch (Exception)
+			{
+				await DisplayAlert("Unable to Dial", "Phone dialing failed.", "OK");
+			}
+		}
+	}
+}
+
